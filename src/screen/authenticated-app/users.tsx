049b6223ad907a19a0d2cc6be792a0ddcbed12de
fangkai <1209683835@qq.com>
2022-03-04 19:07:55 +0800

    feat: 用户数据

--- src/screen/authenticated-app/users.tsx
+++ src/screen/authenticated-app/users.tsx
@@ -1,19 +1,28 @@
 import { toNumber } from "utils";
 import { useUsers } from "service/user";
 
-import { Button, DatePicker, Input, Table, TableProps } from "antd";
+import {
+  Button,
+  DatePicker,
+  Input,
+  Table,
+  TablePaginationConfig,
+  TableProps,
+} from "antd";
 import { Row } from "components/lib";
 import { SearchOutlined } from "@ant-design/icons";
 
 import "moment/locale/zh-cn";
 import locale from "antd/lib/date-picker/locale/zh_CN";
 
-import { User } from "types/user";
+import { User, UsersSearchParams } from "types/user";
 import styled from "@emotion/styled";
 import dayjs from "dayjs";
+import { useState } from "react";
 
 export const Users = () => {
-  const { data, isLoading } = useUsers({ page: 1 });
+  const [params, setParams] = useState<Partial<UsersSearchParams>>({ page: 1 });
+  const { data, isLoading } = useUsers(params);
   const pagination = {
     current: toNumber(data?.page),
     pageSize: toNumber(data?.page_size),
@@ -22,8 +31,10 @@ export const Users = () => {
 
   return (
     <div>
-      <SearchPanel />
+      <SearchPanel params={params} setParams={setParams} />
       <List
+        params={params}
+        setParams={setParams}
         dataSource={data?.list}
         loading={isLoading}
         pagination={pagination}
@@ -32,38 +43,85 @@ export const Users = () => {
   );
 };
 
-const SearchPanel = () => {
+interface SearchPanelProps {
+  params: Partial<UsersSearchParams>;
+  setParams: (params: Partial<UsersSearchParams>) => void;
+}
+
+const SearchPanel = ({ params, setParams }: SearchPanelProps) => {
+  const [temporaryParams, setTemporaryParams] = useState<
+    Partial<UsersSearchParams>
+  >({});
+  const setDates = (dates: any) => {
+    setTemporaryParams({
+      ...temporaryParams,
+      s_time: dates ? `${dayjs(dates[0]).valueOf()}` : "",
+      e_time: dates ? `${dayjs(dates[1]).valueOf()}` : "",
+    });
+  };
+  const setNicename = (evt: any) => {
+    setTemporaryParams({
+      ...temporaryParams,
+      nickname: evt.target.value,
+    });
+  };
+
   return (
     <Row marginBottom={2} gap={true}>
       <Row>
         <div>注册时间：</div>
-        <DatePicker.RangePicker locale={locale} />
+        <DatePicker.RangePicker locale={locale} onChange={setDates} />
       </Row>
       <Row>
         <div>微信昵称：</div>
-        <Input style={{ width: "20rem" }} placeholder="请输入微信昵称" />
+        <Input
+          style={{ width: "20rem" }}
+          onChange={setNicename}
+          placeholder="请输入微信昵称"
+          allowClear={true}
+        />
       </Row>
-      <Button type={"primary"} icon={<SearchOutlined />}>
+      <Button
+        type={"primary"}
+        icon={<SearchOutlined />}
+        onClick={() => setParams({ ...params, ...temporaryParams })}
+      >
         查询
       </Button>
     </Row>
   );
 };
 
-interface ListProps extends TableProps<User> {}
+interface ListProps extends TableProps<User>, SearchPanelProps {}
 
-const List = (props: ListProps) => {
+const List = ({ params, setParams, ...restProps }: ListProps) => {
+  const setPagination = (pagination: TablePaginationConfig) => {
+    setParams({
+      ...params,
+      page: pagination.current,
+      page_size: pagination.pageSize,
+    });
+  };
+
   return (
     <Table
       rowKey={"id"}
       columns={[
         {
           title: "编号",
-          render: (value, user, index) => `${index + 1}`,
+          render: (value, user, index) =>
+            params
+              ? `${
+                  index +
+                  1 +
+                  ((params.page ? params.page : 1) - 1) *
+                    (params.page_size ? params.page_size : 10)
+                }`
+              : 1,
         },
         {
           title: "微信头像",
-          render: (value, user, index) => <Avatar src={user.avatar_url} />,
+          render: (value, user) => <Avatar src={user.avatar_url} />,
         },
         {
           title: "微信昵称",
@@ -71,28 +129,31 @@ const List = (props: ListProps) => {
         },
         {
           title: "性别",
-          render: (value, user, index) => (
-            <span>{user.gender === "1" ? "男" : "女"}</span>
+          render: (value, user) => (
+            <span>
+              {user.gender === "0" ? "未知" : user.gender === "1" ? "男" : "女"}
+            </span>
           ),
         },
         {
           title: "地区",
-          dataIndex: "province",
+          render: (value, user) => <span>{user.province || "无"}</span>,
         },
         {
           title: "注册时间",
-          render: (value, user, index) => (
+          render: (value, user) => (
             <span>
               {user.created_at
                 ? dayjs(Number(user.created_at) * 1000).format(
-                    "YYYY-MM-DD hh:mm"
+                    "YYYY-MM-DD HH:mm"
                   )
                 : "无"}
             </span>
           ),
         },
       ]}
-      {...props}
+      onChange={setPagination}
+      {...restProps}
     />
   );
 };
