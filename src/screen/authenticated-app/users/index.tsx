d5a3678c00432d5a5bac5c16c2c9f680ef749f67
fangkai <1209683835@qq.com>
2022-03-06 01:21:01 +0800

    feat: 入会申请

--- src/screen/authenticated-app/users/index.tsx
+++ src/screen/authenticated-app/users/index.tsx
@@ -12,23 +12,32 @@ export const Users = () => {
 
   return (
     <Container>
-      <SearchPanel params={params} setParams={setParams} />
-      <List
-        error={error}
-        params={params}
-        setParams={setParams}
-        dataSource={data?.list}
-        loading={isLoading}
-        pagination={{
-          current: toNumber(data?.page),
-          pageSize: toNumber(data?.page_size),
-          total: toNumber(data?.total),
-        }}
-      />
+      <Main>
+        <SearchPanel params={params} setParams={setParams} />
+        <List
+          error={error}
+          params={params}
+          setParams={setParams}
+          dataSource={data?.list}
+          loading={isLoading}
+          pagination={{
+            current: toNumber(data?.page),
+            pageSize: toNumber(data?.page_size),
+            total: toNumber(data?.total),
+          }}
+        />
+      </Main>
     </Container>
   );
 };
 
 const Container = styled.div`
+  position: relative;
+  height: 100%;
+`;
+
+const Main = styled.div`
   padding: 2.4rem;
+  height: 100%;
+  overflow: scroll;
 `;
