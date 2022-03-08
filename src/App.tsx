bd51d7f54fdced67a40de0654eff2f3b665b623e
fangkai <1209683835@qq.com>
2022-03-08 21:58:22 +0800

    feat: errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { useAuth } from "context/auth-context";
 import { LoginScreen } from "screen/login";
 import { AuthenticatedApp } from "./screen/authenticated-app/index";
 import "./App.css";
+import { FullPageErrorFallback } from "components/lib";
+import { ErrorBoundary } from "components/error-boundary";
 
 const App = () => {
   const { token } = useAuth();
   return (
-    <div className="App">{token ? <AuthenticatedApp /> : <LoginScreen />}</div>
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {token ? <AuthenticatedApp /> : <LoginScreen />}
+      </ErrorBoundary>
+    </div>
   );
 };
 
