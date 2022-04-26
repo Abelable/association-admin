3414519ad8446575654fcb18fc109ce75be1cd50
fangkai <1209683835@qq.com>
2022-04-26 12:02:47 +0800

    feat: 表单编辑器

--- src/screen/authenticated-app/custom-signups/components/form-builder.tsx
+++ src/screen/authenticated-app/custom-signups/components/form-builder.tsx
@@ -1,9 +1,9 @@
-import { Button, Input, Table } from "antd";
+import { Button, Input, Select, Switch, Table } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
 export const FormBuilder = () => {
   interface FormItem {
-    type: number;
+    type: number | undefined;
     required: boolean;
     name: string;
     tips: string;
@@ -11,33 +11,44 @@ export const FormBuilder = () => {
   }
 
   const defaultFormItem: FormItem = {
-    type: 0,
+    type: undefined,
     required: false,
     name: "",
     tips: "",
     options: undefined,
   };
 
+  const typeOptions = [
+    { id: 1, name: "单行文本框" },
+    { id: 2, name: "多行文本框" },
+    { id: 3, name: "数字输入框" },
+    { id: 4, name: "单选按钮框" },
+    { id: 5, name: "多选按钮框" },
+    { id: 6, name: "日期选择框" },
+    { id: 7, name: "图片上传框" },
+    { id: 8, name: "富文本框" },
+  ];
+
   const defaultFormList: FormItem[] = [
     {
       type: 1,
       required: true,
       name: "姓名",
-      tips: "请输入姓名",
+      tips: "",
       options: undefined,
     },
     {
       type: 1,
       required: true,
       name: "手机号",
-      tips: "请输入手机号",
+      tips: "",
       options: undefined,
     },
     {
       type: 1,
       required: true,
       name: "邮箱",
-      tips: "请输入邮箱",
+      tips: "",
       options: undefined,
     },
   ];
@@ -51,19 +62,36 @@ export const FormBuilder = () => {
           {
             title: "选择类型",
             dataIndex: "type",
+            render: (value, item) => (
+              <Select value={item.type} placeholder="请选择类型">
+                {typeOptions.map((option) => (
+                  <Select.Option key={option.id} value={option.id}>
+                    {option.name}
+                  </Select.Option>
+                ))}
+              </Select>
+            ),
+            width: "16rem",
           },
           {
             title: "选择必填",
             dataIndex: "required",
+            render: (value, item) => <Switch checked={item.required} />,
+            width: "10rem",
           },
           {
             title: "填写名称",
             dataIndex: "name",
-            render: () => <Input placeholder="请输入当前单行文本框的名称" />,
+            render: (value, item) => (
+              <Input value={item.name} placeholder="请输入名称" />
+            ),
           },
           {
             title: "填写提示文本",
             dataIndex: "tips",
+            render: (value, item) => (
+              <Input value={item.tips} placeholder="请输入提示文本" />
+            ),
           },
           {
             title: "添加选项",
