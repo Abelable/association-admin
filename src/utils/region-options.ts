c7de0ecc64f854e406e79b36a787908b0b209e11
Administrator <admin@example.com>
2022-08-23 16:47:44 +0800

    feat: 人才管理

--- src/utils/region-options.ts
+++ src/utils/region-options.ts
@@ -3,39 +3,6 @@ import cities from "china-division/dist/cities.json";
 import areas from "china-division/dist/areas.json";
 import _ from "lodash";
 
-// areas.forEach((area) => {
-//   const matchCity = cities.filter((city) => city.code === area.cityCode)[0];
-//   if (matchCity) {
-//     (matchCity as any).children = (matchCity as any).children || [];
-//     (matchCity as any).children.push({
-//       label: area.name,
-//       value: area.code,
-//     });
-//   }
-// });
-
-// cities.forEach((city) => {
-//   const matchProvince = provinces.filter(
-//     (province) => province.code === city.provinceCode
-//   )[0];
-//   if (matchProvince) {
-//     (matchProvince as any).children = (matchProvince as any).children || [];
-//     (matchProvince as any).children.push({
-//       label: city.name,
-//       value: city.code,
-//       children: (city as any).children,
-//     });
-//   }
-// });
-
-// const options = provinces.map((province) => ({
-//   label: province.name,
-//   value: province.code,
-//   children: (province as any).children,
-// }));
-
-// export default options;
-
 const getOptions = () => {
   const _provinces = _.cloneDeep(provinces);
   const _cities = _.cloneDeep(cities);
