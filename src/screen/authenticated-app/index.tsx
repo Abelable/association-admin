04ca39b25ac468d52fbd8c2fa0ec8cfc67a63c12
Administrator <admin@example.com>
2022-09-13 14:41:41 +0800

    feat: 更新数据大屏连接

--- src/screen/authenticated-app/index.tsx
+++ src/screen/authenticated-app/index.tsx
@@ -166,7 +166,7 @@ const MenuSider = ({ collapsed }: { collapsed: boolean }) => {
           <div
             onClick={() =>
               window.open(
-                "https://datav.aliyuncs.com/share/affdc3921c1cece7b420dbf2c9e414d9"
+                "http://datav.aliyuncs.com/share/e89ac1b2ce4441ccc543fb59de29c04e"
               )
             }
           >
